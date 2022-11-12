18b59495003c764b9d7018eaa8e80e2e3dc14074
xieest <77807321+xieest@users.noreply.github.com>
2022-11-12 17:40:51 -0500

    Update RatingRepository.java

--- src/main/java/com/example/demo/repositories/RatingRepository.java
+++ src/main/java/com/example/demo/repositories/RatingRepository.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Set;
 
+// The repository annotation and the existing JpaRepository class comes with powerful, easy-to-implement tools that helps us easily create methods to retrieve, add, update and delete things on our database
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
+    // The existing framework is robust; simply naming the method 'findAllByBook' allows Spring / JpaRepository to intuit how the method should work.
     List<Rating> findAllByBook(Book book);
 
     Rating save(Rating rating);
