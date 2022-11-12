acbae021d52638aebf9af1bbbd33e5d737712292
xieest <77807321+xieest@users.noreply.github.com>
2022-11-12 17:59:18 -0500

    Add files via upload

---
+++ src/test/java/com/example/demo/controllers/RatingControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Rating;
+import com.example.demo.services.RatingService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RatingControllerTest {
+
+    RatingService ratingService = new RatingService();
+
+    @Test
+    void getAllRatings() {
+    }
+
+    @Test
+    void saveRating() {
+    }
+
+    @Test
+    void testGetAllRatings() {
+    }
+
+    @Test
+    void getRatingsSorted() {
+    }
+
+    @Test
+    void findAverageRatingByBook() {
+    }
+
+    @Test
+    void findRatingsByBook() {
+    }
+}
---
+++ src/test/java/com/example/demo/services/RatingServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RatingServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findAllByBook() {
+    }
+
+    @Test
+    void saveRating() {
+    }
+}
