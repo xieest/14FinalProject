78ebb5b5aa2ea3d3c0ba235f41500a334ba0460c
xieest <77807321+xieest@users.noreply.github.com>
2022-11-12 17:49:30 -0500

    Update RatingController.java

--- src/main/java/com/example/demo/controllers/RatingController.java
+++ src/main/java/com/example/demo/controllers/RatingController.java
@@ -25,6 +25,9 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
 
+    // The client must provide a rating object formatted correctly with all necessary fields. As long as the rating number is a valid integer (in our case between 1 and 5 inclusive) the rating is saved in our database
+    // Otherwise the client is given a http response with a custom header that says the rating is invalid.
+
     @ApiOperation(value = "save new rating to the database")
     @PostMapping(value = {"", "/"})
     public ResponseEntity<Rating> saveRating(@RequestBody Rating rating) {
@@ -39,12 +42,16 @@ public class RatingController {
             return new ResponseEntity<>((Rating) null, responseHeaders, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
+    // This returns all ratings from the database.
+
     @ApiOperation(value = "return all ratings from database")
     @GetMapping(value = {"", "/"})
     public ResponseEntity<List<Rating>> getAllRatings(){
 
         return new ResponseEntity<>(ratingService.findAll(), HttpStatus.OK);
     }
+
+    // This returns all ratings from the database sorted by rating value (highest to lowest)
 
     @GetMapping("/getRatingsSorted")
         public List<Rating> getRatingsSorted() {
@@ -55,6 +62,7 @@ public class RatingController {
             return sortedRatings;
     }
 
+    // This returns the average rating for a specific book; the book ISBN needs to be provided in the URI
 
     @ApiOperation(value = "return average rating for an individual book")
     @GetMapping(value = {"/{bookISBN}/average"})
@@ -70,6 +78,8 @@ public class RatingController {
                 .average();
         return average.isPresent() ? average.getAsDouble() : null;
     }
+
+    // This returns all ratings for a specific book; the book ISBN needs to be provided in the URI
 
     @ApiOperation(value = "return all ratings for an individual book")
     @GetMapping("/{bookISBN}")
