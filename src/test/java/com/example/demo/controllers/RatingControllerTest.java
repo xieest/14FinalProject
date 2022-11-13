6650097312aafedc4955869e18d4a1bf35fda804
xieest <77807321+xieest@users.noreply.github.com>
2022-11-12 21:03:12 -0500

    Update RatingControllerTest.java

--- src/test/java/com/example/demo/controllers/RatingControllerTest.java
+++ src/test/java/com/example/demo/controllers/RatingControllerTest.java
@@ -10,10 +10,12 @@ import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -26,16 +28,13 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RatingControllerTest {
-
-    @Mock
-    private RatingRepository ratingRepository;
-
-    @Mock
-    private BookRepository bookRepository;
 
     @Mock
     private RatingService ratingService;
@@ -46,6 +45,10 @@ class RatingControllerTest {
     Rating rating1 = new Rating();
     Rating rating2 = new Rating();
     Rating rating3 = new Rating();
+
+    Book book1 = new Book();
+
+    User user1 = new User();
 
     @BeforeEach
     public void setup() throws IOException {
@@ -80,54 +83,86 @@ class RatingControllerTest {
     @Test
     void saveRating() {
 
+        when(ratingService.saveRating(any(Rating.class))).then(AdditionalAnswers.returnsFirstArg());
         ResponseEntity<Rating> first = ratingController.saveRating(rating1);
         assertThat(first.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         ResponseEntity<Rating> second = ratingController.saveRating(rating2);
+        System.out.println(second.getStatusCode());
+        System.out.println(second.getBody());
         assertThat(second.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         // Ensuring that if the rating is not between 1 and 5, the rating controller does not create the resource
         ResponseEntity<Rating> third = ratingController.saveRating(rating3);
+        System.out.println(third.getStatusCode());
         assertThat(third.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @Test
     void getAllRatings() {
+
+        when(ratingService.saveRating(any(Rating.class))).then(AdditionalAnswers.returnsFirstArg());
         Rating saved1 = ratingService.saveRating(rating1);
         Rating saved2 = ratingService.saveRating(rating2);
+
         List<Rating> ratings = new ArrayList<>();
         ratings.add(saved1);
         ratings.add(saved2);
 
         given(ratingService.findAll()).willReturn(ratings);
         List<Rating> retrievedRatings = ratingController.getAllRatings().getBody();
+        System.out.println(retrievedRatings.get(0).toString());
         assertThat(retrievedRatings).isNotNull();
 
         assertEquals(HttpStatus.OK, ratingController.getAllRatings().getStatusCode());
     }
 
-    @Test //Still working on this test
+    @Test
     void getRatingsSorted() {
-        Rating saved1 = ratingService.saveRating(rating1);
-        Rating saved2 = ratingService.saveRating(rating2);
+
+        book1 = new Book();
+        book1.setBookISBN(1234124112);
+        book1.setBookAuthor("Some author");
+        book1.setBookDescription("Some description");
+        book1.setBookGenre("Some genre");
+        book1.setBookName("Some book name");
+        book1.setBookPrice(40.20);
+        book1.setBookPublisher("Some publisher");
+        book1.setBookCataloguePosition(1);
+        book1.setBooksSold(1234);
+        book1.setBookYear(2022);
+
+        user1.setUserName("email here");
+        user1.setFirstName("Tester");
+        user1.setLastName("Ace");
+        user1.setPassword("Password");
+        user1.setHomeAddress("Address");
+
+        rating1.setUser(user1);
+        rating1.setBook(book1);
+
+        rating2.setUser(user1);
+        rating2.setBook(book1);
+
+        when(ratingService.saveRating(any(Rating.class))).then(AdditionalAnswers.returnsFirstArg());
+        Rating saved1 = ratingController.saveRating(rating1).getBody();
+        Rating saved2 = ratingController.saveRating(rating2).getBody();
+
         List<Rating> ratings = new ArrayList<>();
         ratings.add(saved1);
         ratings.add(saved2);
 
-//        List<Rating> sortedRatings = new ArrayList<>();
-//        sortedRatings.add(saved1);
-//        sortedRatings.add(saved2);
-        Collections.sort(ratings);
+        List<Rating> sortedRatings = new ArrayList<>();
+        sortedRatings.add(saved1);
+        sortedRatings.add(saved2);
 
-        given(ratingService.findAll()).willReturn(ratings);
-        given(ratingController.getRatingsSorted()).willReturn(new ResponseEntity<>(ratings, HttpStatus.OK));
+        given(ratingService.findAll()).willReturn(sortedRatings);
         List<Rating> retrievedRatings = ratingController.getRatingsSorted().getBody();
         assertThat(retrievedRatings).isNotNull();
 
-        retrievedRatings.forEach(rating -> System.out.println(rating.getComment()));
+        // Checking to see the list of ratings was properly sorted
 
         assertEquals(HttpStatus.OK, ratingController.getAllRatings().getStatusCode());
-        System.out.println(retrievedRatings.get(0).toString());
         assertEquals(5, retrievedRatings.get(0).getRating());
         assertEquals(4, retrievedRatings.get(1).getRating());
         assertEquals(4, ratings.get(0).getRating());
