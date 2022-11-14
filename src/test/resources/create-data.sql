91918f36bc77a339b8b260fe83aa15bc6cc79ef0
xieest <77807321+xieest@users.noreply.github.com>
2022-11-14 18:47:28 -0500

    Update create-data.sql

--- src/test/resources/create-data.sql
+++ src/test/resources/create-data.sql
@@ -1,3 +1,5 @@
+DELETE FROM booksmanagement.ratings;
+
 INSERT INTO booksmanagement.ratings (id, rating, comment, datestamp, user, book) VALUES
     (1, 3, 'This here is the first test comment', '2022-11-13', 'some_email_here', 9711161484100),
     (2, 5, 'This here is the second test comment', '2022-11-13', 'some_email_here', 9711161484100),
