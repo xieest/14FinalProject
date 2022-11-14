6a306e3fd16b8c0a412d722e62566765fefb35e7
xieest <77807321+xieest@users.noreply.github.com>
2022-11-14 18:54:00 -0500

    Delete src directory

--- src/test/resources/create-data.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM booksmanagement.ratings;
-
-INSERT INTO booksmanagement.ratings (id, rating, comment, datestamp, user, book) VALUES
-    (1, 3, 'This here is the first test comment', '2022-11-13', 'some_email_here', 9711161484100),
-    (2, 5, 'This here is the second test comment', '2022-11-13', 'some_email_here', 9711161484100),
-    (3, 5, 'This here is the third test comment', '2022-11-13', 'some_email_here', 9711161484100);
-
